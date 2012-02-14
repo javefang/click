1a450c0f71efa03eab07708e9765b9cee31b38b7
Xinghong Fang <xf214@cam.ac.uk>
2012-02-14 20:45:34 +0000

    build script simplified.

--- ndk-configure-click.sh
+++ ndk-configure-click.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-SYSROOT="$MY_NDK/sysroot"
-LIBGCC=`arm-linux-androideabi-gcc -print-libgcc-file-name`
+SYSROOT="$MY_NDK/sysroot" \
+CC="arm-linux-androideabi-gcc" \
+CXX="arm-linux-androideabi-g++" \
 ./configure --host=arm-linux-androideabi \
 --prefix=/data/click \
-CPPFLAGS="-I$SYSROOT/usr/include" \
 CXXFLAGS="-fno-rtti -fno-exceptions" \
-LDFLAGS="-Wl,-rpath-link=$SYSROOT/usr/lib,-dynamic-linker=/system/bin/linker" \
-LIBS="-lm -lstdc++ -lc $LIBGCC" \
 --disable-linuxmodule --enable-tools=host
