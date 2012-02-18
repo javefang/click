cbfcc0ee7ceb0ef0e0c0bf22a7840d8aae5434a8
Xinghong Fang <xf214@cam.ac.uk>
2012-02-18 18:51:50 +0000

    build script updated, now link to gnustl by default, LD_LIBRARY_PATH needs to be set before executing the program

--- ndk-configure-click.sh
+++ ndk-configure-click.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-SYSROOT="$MY_NDK/sysroot" \
 CC="arm-linux-androideabi-gcc" \
 CXX="arm-linux-androideabi-g++" \
 ./configure --host=arm-linux-androideabi \
 --prefix=/data/click \
-CXXFLAGS="-fno-rtti -fno-exceptions" \
+CXXFLAGS="-fno-rtti -fno-exceptions -g" \
+LDFLAGS="-lgnustl_shared" \
 --disable-linuxmodule --enable-tools=host
