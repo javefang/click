4e36bdf12076016204638b97b6a22ce2b56c9cd1
Xinghong Fang <xinghong.fang@gmail.com>
2012-06-24 17:26:07 +0100

    configure and make script updated

--- ndk-configure-click.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-CC="arm-linux-androideabi-gcc" \
-CXX="arm-linux-androideabi-g++" \
-./configure --host=arm-linux-androideabi \
---prefix=/data/click \
-CXXFLAGS="-fno-rtti -fno-exceptions -g" \
-LDFLAGS="-lgnustl_shared" \
---disable-linuxmodule --enable-tools=host
